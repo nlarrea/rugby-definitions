a797a908a26732f72392ca0888cfcaa23b8a3706
nlarrea <larrea.naia@gmail.com>
2024-12-30 17:28:55 +0100

    Add color constants and layout component for theming support

---
+++ frontend/src/constants/colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+	darkBg: 'hsl(220, 19.60%, 21.00%)', //#2b3240
+	darkText: 'hsl(205, 40.70%, 89.40%)', //#d9e6ef
+	darkAccent: 'hsl(29, 96.40%, 56.30%)', //#fb8b24
+	lightBg: 'hsl(75, 6.20%, 87.50%)', //#e0e1dd
+	lightText: 'hsl(221, 32.60%, 16.90%)', //#1d2639
+	lightAccent: 'hsl(195, 52.50%, 42.90%)', //#348aa7
+};
