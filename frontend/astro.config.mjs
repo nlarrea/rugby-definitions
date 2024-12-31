8c588714fcd9769c024be34085402d3313455b5a
nlarrea <larrea.naia@gmail.com>
2024-12-31 16:46:41 +0100

    Add React support and update dependencies in Astro configuration

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
 	i18n: {
@@ -14,4 +16,6 @@ export default defineConfig({
 			en: 'es',
 		},
 	},
+
+	integrations: [react({ include: '**/react/*' })],
 });
