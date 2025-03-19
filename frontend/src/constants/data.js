a6b956517d9b2d82f5b77061baef3cfc61e5f075
nlarrea <larrea.naia@gmail.com>
2025-03-19 23:44:44 +0100

    feat: add bento items section to About page with internationalization support

---
+++ frontend/src/constants/data.js
@@ -0,0 +1,30 @@
+export const bentoItems = {
+	courses: {
+		link: 'https://passport.world.rugby/officiating/?overridelang=1',
+		image: 'https://passport.world.rugby/media/s1xfofdx/rollup-law-exam.png',
+	},
+	laws: {
+		link: 'https://passport.world.rugby/laws-of-the-game/laws-by-number/1-the-ground/',
+		image: 'https://passport.world.rugby/media/0qydjle3/25en-rollup-laws-by-number-en.png',
+	},
+	variations: {
+		link: 'https://passport.world.rugby/laws-of-the-game/variations-7s-10s-u19-game-on/under-19/3-team/',
+		image: 'https://passport.world.rugby/media/a2wdw2pt/rollup_variations.png',
+	},
+	definitions: {
+		link: 'https://passport.world.rugby/laws-of-the-game/definitions/',
+		image: 'https://passport.world.rugby/media/yykbssru/laws_definitions_opt3.png',
+	},
+	signals: {
+		link: 'https://passport.world.rugby/laws-of-the-game/match-official-signals/primary-referee-signals/',
+		image: 'https://passport.world.rugby/media/gzwdnwj0/roll-up-signals.png',
+	},
+	clarifications: {
+		link: 'https://passport.world.rugby/laws-of-the-game/law-clarifications/?overridelang=1',
+		image: 'https://passport.world.rugby/media/gfjj0vrp/rollup_law_clarifications.png',
+	},
+	news: {
+		link: 'https://passport.world.rugby/laws-of-the-game/laws-news/',
+		image: 'https://passport.world.rugby/media/g4xpxkrx/laws_news_updates_220x163.png',
+	},
+};
